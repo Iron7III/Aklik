2b0995b95fc37ddd5eb10485051d444c90cd13b1
unknown <aaronsjimenez@gmail.com>
2020-11-14 22:39:22 +0100

    x

--- src/command/status.js
+++ src/command/status.js
@@ -6,6 +6,7 @@ const client = new Discord.Client({
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message, args, Fortnite) => {
+if(message.author.id!==438390132538605589) return;
 if(!args[0]) return;
 client.user.setActivity(args[0], { type: "WATCHING" });
 message.channel.send(`ESTADO CAMBIADO CORRECTAMENTE`);
