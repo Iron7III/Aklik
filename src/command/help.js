38a4328654d574a795a8d58077b2c3d42008117c
unknown <aaronsjimenez@gmail.com>
2020-11-14 21:12:26 +0100

    x

--- src/command/help.js
+++ src/command/help.js
@@ -29,7 +29,7 @@ let CommandsArray=[
   ``,
 ];
 let InformationArray=[
-  `${client.emojis.cache.get("769568936077033482")} **UPTIME\n- **\`${ConvertedUptime}\``,
+  `${client.emojis.cache.get("769568936077033482")} **UPTIME\n- **\`${ConvertedUptime.toFixed(1)}\``,
   `${client.emojis.cache.get("768962558144938014")} **SERVERS\n- **\`${client.guilds.cache.size}\``,
   `**LENGUAJE\n- **\`Node.js\` ${client.emojis.cache.get("777182228987772958")}`,
   `${client.emojis.cache.get("768962690508783646")} **LIBRERIAS\n- **\`discord.js\` ${client.emojis.cache.get("777189460655341600")}\n**- **\`axios\` ${client.emojis.cache.get("777189460655341600")}\n**- **\`express\` ${client.emojis.cache.get("777189460655341600")}\n**- **\`fortnite-api-com\` ${client.emojis.cache.get("777189460655341600")}`,
