e1f22dc6547f6ad00e7cdc2a5f74cc74b8685f23
Iron [Aarón] <aaronsjimenez@gmail.com>
2020-12-06 20:17:01 +0100

    aadada

--- src/command/test.js
+++ src/command/test.js
@@ -20,12 +20,20 @@ exports.run = async (client, message, args, Fortnite) => {
                 ctx.drawImage(image, 0, 0, 512, 512)
                 loadImage(`https://cdn.glitch.com/e0a62737-efb4-4dbe-9768-8579bd913488%2Fcard_bottom_${res.data.rarity.value}.png?v=1603671079787`).then((image) => {
                     ctx.drawImage(image, 0, 81, 512, 431)
+                    // COSMETIC NAME
                     ctx.fillStyle = 'white'
                     registerFont('Burbank Big Condensed Black 700.ttf', { family: 'Burbank Big Condensed Black' })
                     ctx.font = '30px Burbank Big Condensed'
                     var n = res.data.name;
                     var t = ctx.measureText(n);
                     ctx.fillText(n, (512-t.width)/2, 450)
+                    // COSMETIC PRICE
+                    ctx.fillStyle = 'white'
+                    registerFont('Burbank Big Condensed Black 700.ttf', { family: 'Burbank Big Condensed Black' })
+                    ctx.font = '36px Burbank Big Condensed'
+                    var n = res.data.finalPrice;
+                    var t = ctx.measureText(n);
+                    ctx.fillText(n, (512-t.width)/2, 500)
                     const attach = new Discord.MessageAttachment(canvas.toBuffer(), 'cosmetic.png')
                     message.channel.send(attach)
 /*
