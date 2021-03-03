3b20049f81dc63dac12cec9eb2c9589d72934b08
Iron7III <aaronsjimenez@gmail.com>
2021-03-03 19:54:25 +0100

    awdaw

--- src/command/custom.js
+++ src/command/custom.js
@@ -23,7 +23,8 @@ exports.run = async (client, message, args, Fortnite) => {
         .then(() => {
             Custom.setTitle('**PARTIDA CUSTOM**')
             Custom.setDescription(`**MODO :: **\`${m[args[0]]}\`\n**CODIGO :: **\`${p}\``)
-        }).setInterval(() => {
-            message.edit({embed:Custom})
-        },3000);
+        })
+    .setTimeout(function(){ 
+        message.edit({embed:Custom})
+    }, 3000);
 };
