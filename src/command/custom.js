d3ec3d8d0acab8e3b0c76ec5272904d3a223f880
Iron7III <aaronsjimenez@gmail.com>
2021-03-03 19:39:54 +0100

    xdddd

--- src/command/custom.js
+++ src/command/custom.js
@@ -7,21 +7,24 @@ const client = new Discord.Client({
 });
 
 exports.run = async (client, message, args, Fortnite) => {
+    message.delete()
     const Custom = new Discord.MessageEmbed()
-        .setTitle('**GENERANDO CODIGO PARA CUSTOM...**')
+        .setTitle('**GENERANDO CODIGO PARA PARTIDA CUSTOM...**')
         .setColor('#3498db')
     let m = {
         "solo":"Solitario",
         "duo":"Duos",
         "trio":"Trios"
     }
-    var c = 'abcdefghijkmnpqrtuvwxyzABCDEFGHJKMNPQRTUVWXYZ2346789';
+    var c = 'abcdefghijkmnpqrtuvwxyz';
     var p = '';
     for (i=0;i<8;i++) p +=c.charAt(Math.floor(Math.random()*c.length)); 
     message.channel.send({embed:Custom})
-        .then((msg) => {
-            Custom.setTitle('**CUSTOM**')
+        .setTimeout(() => {
+            Custom.setTitle('**PARTIDA CUSTOM**')
             Custom.setDescription(`**MODO :: **\`${m[args[0]]}\`\n**CODIGO :: **\`${p}\``)
-            msg.edit({embed:Custom})
+            setInterval(()=>{
+                msg.edit({embed:Custom})
+            },3000)
         })
 }
