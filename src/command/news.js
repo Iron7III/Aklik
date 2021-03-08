f141c6a9249e91eb4732786c95d526f6017da3ba
Iron7III <aaronsjimenez@gmail.com>
2021-03-08 15:14:03 +0100

    a

--- src/command/news.js
+++ src/command/news.js
@@ -77,13 +77,13 @@ exports.run = async (client, message, args, Fortnite) => {
 Fortnite.News('es').then(res => {
 const canvas = createCanvas(1920*res.data.br.motds.length,1080)
 const ctx = canvas.getContext('2d')
+ctx.fillStyle = '#ffffff';
 ctx.fillRect(0, 0, 1920*res.data.br.motds.length,1080)
 loadImage(res.data.br.motds[0].image).then((image) => {
     ctx.drawImage(image, 0, 0)
 loadImage(res.data.br.motds[1].image).then((image) => {
     ctx.drawImage(image, 1920, 0)
-})})
 const attach = new Discord.MessageAttachment(canvas.toBuffer(), `news.jpg`)
 message.channel.send(attach)
-});
+})})});
 };
