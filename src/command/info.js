0decc11bebe24a06f5a6a9a042472561cf21dbdf
Iron7III <aaronsjimenez@gmail.com>
2021-04-17 11:52:09 +0200

    a

--- src/command/info.js
+++ src/command/info.js
@@ -41,7 +41,7 @@ exports.run = async (client, message, args, Fortnite) => {
         "servers":{
             "header":`${client.emojis.cache.get("768962558144938014")} **SERVIDORES | USUARIOS**`,
             "list":[
-                `\`${client.guilds.cache.size}\` **|** \`${client.users.size}\``
+                `\`${client.guilds.cache.size}\` **|** \`${client.users.cache.size}\``
             ]
         },
         "idioms":{
