ca7c99716d6f21b880a846b69ab250cd20fe360f
Iron <aaronsjimenez@gmail.com>
2021-05-18 20:45:53 +0200

    a

--- src/command/ban.js
+++ src/command/ban.js
@@ -1,15 +1,10 @@
 //  npm Discord
 const Discord = require("discord.js");
-//  Usuario del Bot de Discord
-const client = new Discord.Client({
-  disableEveryone: true,
-  fetchAllMembers: true
-});
 
 exports.run = async (client, message, args, Fortnite) => {
     const embed = new Discord.MessageEmbed();
     const user = message.mentions.users.first()||args[0];
-    const member = message.guild.member(user);
+    const member = message.guild.member(user.id);
     if(!user){
         embed.setDescription(`_Has de mencionar a un usuario o ID._`).setColor('#ED4245')
         message.channel.send({embed: embed})
@@ -21,11 +16,11 @@ exports.run = async (client, message, args, Fortnite) => {
     if(member){
         member.ban(
             {
-                reason: args[1]?args[1]:'No hay motivo.'
+                reason: args[1]?message.content.slice(args[0]):'No hay motivo.'
             }
         )
         .then(()=>{
-                embed.setDescription(`_${user.tag} ha sido baneado con motivo: \`${args[1]?args[1]:'No hay motivo.'}\`_`).setColor('#57F287')
+                embed.setDescription(`_${user.tag} ha sido baneado con motivo: \`${args[1]?message.content.slice(args[0]):'No hay motivo.'}\`_`).setColor('#57F287')
                 message.channel.send({embed: embed})
             }
         )
