26bbae78a36399987e8f748bf58b80076c406e2f
Iron <aaronsjimenez@gmail.com>
2021-05-19 20:28:17 +0200

    a

--- src/command/kick.js
+++ src/command/kick.js
@@ -9,12 +9,12 @@ const client = new Discord.Client({
     const embed = new Discord.MessageEmbed();
     const user = client.users.cache.get(args[0]);
     console.log(user)
-    const member = message.guild.members.fetch(user.id);
     const reason = args[1]?args.slice(1).join(' '):'No hay motivo.';
     if(!user) {
         embed.setDescription(`_Has de mencionar a un usuario o ID._`).setColor('#ED4245')
         message.channel.send({embed: embed})
     } else {
+        const member = message.guild.members.fetch(user.id);
         if(!member) {
             embed.setDescription(`_El usuario no esta en el servidor._`).setColor('#ED4245')
             message.channel.send({embed: embed})
