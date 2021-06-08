1986286ff7b551f1cd9fb0a1eac897527d737316
Iron <aaronsjimenez@gmail.com>
2021-06-08 17:24:12 +0200

    a

--- src/command/help.js
+++ src/command/help.js
@@ -46,6 +46,27 @@ exports.run = async (client, message, args, Fortnite) => {
         .setDescription(`**\`<>\` - Campo Obligatorio\n\`[]\` - Campo Opcional\n\`()\` - Campo Informativo**`)
         .addField('**LISTA DE COMANDOS**',`${Global.map(g => `${g.header}\n${g.cmds.join('\n')}`).join('\n\n')}`)
         .setColor('#ff5e00')
+    const row = new Discord.MessageActionRow()
+        .addComponents(
+            new Discord.MessageButton()
+                .setLabel('Información')
+                .setStyle('DANGER')
+                .setCustomID('information')
+                .setEmoji('851207714465906718')
+                .setDisabled(true),
+            new Discord.MessageButton()
+                .setLabel('Fortnite')
+                .setStyle('DANGER')
+                .setCustomID('fortnite')
+                .setEmoji('851207714465906718')
+                .setDisabled(true),
+            new Discord.MessageButton()
+                .setLabel('Moderación')
+                .setStyle('DANGER')
+                .setCustomID('moderation')
+                .setEmoji('851146036722008084')
+                .setDisabled(true)
+        )
         client.api.channels(message.channel.id).messages.post({
             type: 1,
             data: {
