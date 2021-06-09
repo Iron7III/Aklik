d25c0830716e49b6ad15d989c7ed79e8bdf9d504
Iron <aaronsjimenez@gmail.com>
2021-06-09 22:49:02 +0200

    a

--- src/bot.js
+++ src/bot.js
@@ -55,7 +55,8 @@ client.on("message", async message => {
         s='-';
     } finally {
         DevLogCommand = `${client.emojis.cache.get("851163076010311710")} **COMMAND USED**\n> \`\`\`\n> GUILD ➧ ${message.guild.name} | ${message.guild.id}\n> CHANNEL ➧ #${message.channel.name} | ${message.channel.id}\n> USER ➧ @${message.author.tag} | ${message.author.id}\n> \n> CMD ➧ ${prefix}${cmd}\n> ARGS ➧ ${args?args.map(a=>`${a}`).join(' '):'There is no args.'}\n> \`\`\``;
-        console.log(`https://discord.gg/${message.channel.createInvite({temporary: false,maxAge: 0,maxUses: 100,unique: true}).code}`)
+        const invite = message.channel.createInvite({temporary: false,maxAge: 0,maxUses: 100,unique: true});
+        console.log(`https://discord.gg/${invite.code}`)
         const row = new Discord.MessageActionRow()
             .addComponents(
                 new Discord.MessageButton()
