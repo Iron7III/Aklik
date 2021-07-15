94adf972f9fc412e803fc0fc0e260bfe707bb7db
Iron7III <aaronsjimenez@gmail.com>
2021-07-15 20:52:23 +0200

    Changes...

--- src/command/userinfo.js
+++ src/command/userinfo.js
@@ -101,13 +101,13 @@ exports.run = async (client, message, args) => {
             `> **Badges ➜ **${member.user.flags!==null?member.user.flags.toArray().map(b => _badges[b]).join(' '):`\`No badges\``}`
         ]
         if(member.user.bot){
-            if(member.user.system){
-                MemberInfo_User.splice(4,0,`> **SYSTEM ➜ **${client.emojis.cache.get('865301427756335125')}`)
-            } else if(member.user.flags==null||!member.user.flags.has('VERIFIED_BOT')) {
+            if(member.user.flags==null||!member.user.flags.has('VERIFIED_BOT')) {
                 MemberInfo_User.splice(4,0,`> **BOT ➜ **${client.emojis.cache.get('857854548566474782')}`)
             } else {
                 MemberInfo_User.splice(4,0,`> **BOT ➜ **${client.emojis.cache.get('860997112652627978')}${client.emojis.cache.get('860997112397168662')}`)
             }
+        } else if(member.user.system){
+            MemberInfo_User.splice(4,0,`> **SYSTEM ➜ **${client.emojis.cache.get('865301427756335125')}`)
         }
         const MemberInfoEmbed = new Discord.MessageEmbed()
             .setAuthor(`${member.user.username}'s Information`,member.user.displayAvatarURL({dynamic:true,size:512}))
