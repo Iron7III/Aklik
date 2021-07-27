7292b4159415a6ab4cd61893fc32d47485198f66
Iron7III <aaronsjimenez@gmail.com>
2021-07-27 13:26:25 +0200

    Change

--- src/command/userinfo.js
+++ src/command/userinfo.js
@@ -97,7 +97,7 @@ exports.run = async (client, message, args) => {
             }
         }
         var UserBadges = User.flags.toArray().map(b => Badges[b]);
-        if(APIUser.avatar.startsWith('a_'||APIUser.banner.startsWith('a_'))){
+        if(APIUser.avatar.startsWith('a_')||APIUser.banner.startsWith('a_')){
             UserBadges.push(client.emojis.cache.get('867772736651001926'))
         }
         var UserInfo = [
