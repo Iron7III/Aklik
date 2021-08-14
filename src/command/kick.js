6c9010df94fa85ecd281a609918b25cffe9b9f2d
Iron7III <aaronsjimenez@gmail.com>
2021-08-15 01:55:12 +0200

    Ultra fast fix

--- src/command/kick.js
+++ src/command/kick.js
@@ -5,17 +5,17 @@ exports.run = async (client, message, args, FortniteAPIComClient, FortniteAPIIoC
     const embed = new Discord.MessageEmbed();
     if(!args[0]){
         embed.setAuthor(`Mention a user or write a valid ID.`,assets.mention).setColor('#ED4245')
-        message.channel.send({embed: embed})
+        message.channel.send({embeds: [embed]})
             .then(msg => client.setTimeout(() => msg.delete(), 5000))
     } else {
         let user = message.mentions.users.first() || client.users.resolve(args[0]) || message.guild.members.cache.find(m => m.user.username.toLowerCase() == args[0]) || await client.users.fetch(args[0]);
         if(!user||user.id==message.author.id){
-            message.channel.send({embed: embed})
+            message.channel.send({embeds: [embed]})
                 .then(msg => client.setTimeout(() => msg.delete(), 5000))
         } else {
             if(!message.member.permissions.has('KICK_MEMBERS')){
                 embed.setDescription(`_No tienes permisos para kickear usuarios._`).setColor('#ED4245')
-                message.channel.send({embed: embed})
+                message.channel.send({embeds: [embed]})
                     .then(msg => client.setTimeout(() => msg.delete(), 5000))
             } else {
                 const member = message.guild.members.cache.get(user.id);
@@ -24,23 +24,23 @@ exports.run = async (client, message, args, FortniteAPIComClient, FortniteAPIIoC
                 console.log(member)
                 if(!member){
                     embed.setDescription(`_El usuario no esta en el servidor._`).setColor('#ED4245')
-                    message.channel.send({embed: embed})
+                    message.channel.send({embeds: [embed]})
                         .then(msg => client.setTimeout(() => msg.delete(), 5000))
                 } else {
                     if(!member.kickable){
                         embed.setDescription(`_El usuario no puede ser kickeado._`).setColor('#ED4245')
-                        message.channel.send({embed: embed})
+                        message.channel.send({embeds: [embed]})
                             .then(msg => client.setTimeout(() => msg.delete(), 5000))
                     } else {
                         const reason = args.slice(1).join(' ')?args.slice(1).join(' '):'No se ha especificado una razón.';
                         member.kick(reason)
                             .then(()=>{
                                 embed.setDescription(`_${member.user.username}#${member.user.discriminator} ha sido kickeado con motivo: \`${reason}\`_`).setColor('#57F287')
-                                message.channel.send({embed: embed})
+                                message.channel.send({embeds: [embed]})
                             })
                             .catch(err => {
                                 embed.setDescription(`_No puedo kickear a este usuario._`).setColor('#ED4245')
-                                message.channel.send({embed: embed})
+                                message.channel.send({embeds: [embed]})
                                     .then(msg => client.setTimeout(() => msg.delete(), 5000))
                                 console.error(err);
                             })
