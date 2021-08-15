7df1b46336c63f1ecae5512e78a79bd2066cb09e
Iron7III <aaronsjimenez@gmail.com>
2021-08-15 13:03:35 +0200

    some changes

--- src/bot.js
+++ src/bot.js
@@ -52,8 +52,8 @@ client.on("ready", async () => {
     const { generateShop, getShopItems } = require("./shop");
     const { apiKey, language, watermark } = require("./config.json");
     (async () => {
-        //const items = await getShopItems(apiKey, language);
-        //await generateShop(items, watermark);
+        const items = await getShopItems(apiKey, language);
+        await generateShop(items, watermark);
     })()
 })
 
--- src/command/ban.js
+++ src/command/ban.js
@@ -1,45 +1,53 @@
 const Discord = require("discord.js");
 
-exports.run = async (client, message, args, FortniteAPIComClient, FortniteAPIIoClient, {assets}) => {
+exports.run = async (client, message, args, FortniteAPIComClient,FortniteAPIIoClient, {assets}, checkSnowflakeId) => {
     const embed = new Discord.MessageEmbed();
     if(!args[0]){
         embed.setAuthor(`Mention a user or write a valid ID.`,assets.mention).setColor('#ED4245')
         message.channel.send({embeds: [embed]})
             .then(msg => setTimeout(() => msg.delete(), 5000))
     } else {
-        let user = message.mentions.users.first() || client.users.resolve(args[0]) || message.guild.members.cache.find(m => m.user.username.toLowerCase() == args[0]) || await client.users.fetch(args[0]);
-        if(!user||user.id==message.author.id){
-            message.channel.send({embeds: [embed]})
-                .then(msg => setTimeout(() => msg.delete(), 5000))
+        if(checkSnowflakeId(args[0]?getIdFromMention(args[0])||args[0]:message.author.id)===false){
+            const InvalidSnowflakeUserId = new Discord.MessageEmbed()
+                .setAuthor(`Write a valid ID.`,assets.error)
+                .setColor('#ED4245')
+            message.channel.send({embeds:[InvalidSnowflakeUserId]})
+            return
         } else {
-            if(!message.member.permissions.has('BAN_MEMBERS')){
-                embed.setDescription(`_No tienes permisos para banear usuarios._`).setColor('#ED4245')
+            let user = message.mentions.users.first() || client.users.resolve(args[0]) || message.guild.members.cache.find(m => m.user.username.toLowerCase() == args[0]) || await client.users.fetch(args[0]);
+            if(!user||user.id==message.author.id){
                 message.channel.send({embeds: [embed]})
-                    .then(msg => client.setTimeout(() => msg.delete(), 5000))
+                    .then(msg => setTimeout(() => msg.delete(), 5000))
             } else {
-                const member = message.guild.members.cache.get(user.id);
-                if(!member){
-                    embed.setDescription(`_El usuario no esta en el servidor._`).setColor('#ED4245')
+                if(!message.member.permissions.has('BAN_MEMBERS')){
+                    embed.setDescription(`_No tienes permisos para banear usuarios._`).setColor('#ED4245')
                     message.channel.send({embeds: [embed]})
-                        .then(msg => setTimeout(() => msg.delete(), 5000))
+                        .then(msg => client.setTimeout(() => msg.delete(), 5000))
                 } else {
-                    if(!member.bannable){
-                        embed.setDescription(`_El usuario no puede ser baneado._`).setColor('#ED4245')
+                    const member = message.guild.members.cache.get(user.id);
+                    if(!member){
+                        embed.setDescription(`_El usuario no esta en el servidor._`).setColor('#ED4245')
                         message.channel.send({embeds: [embed]})
                             .then(msg => setTimeout(() => msg.delete(), 5000))
                     } else {
-                        const reason = args.slice(1).join(' ')?args.slice(1).join(' '):'No se ha especificado una razón.';
-                        member.ban({days:7,reason:reason})
-                            .then(()=>{
-                                embed.setDescription(`_${member.user.username}#${member.user.discriminator} ha sido baneado con motivo: \`${reason}\`_`).setColor('#57F287')
-                                message.channel.send({embeds: [embed]})
-                            })
-                            .catch(err => {
-                                embed.setDescription(`_No puedo banear a este usuario._`).setColor('#ED4245')
-                                message.channel.send({embeds: [embed]})
-                                    .then(msg => setTimeout(() => msg.delete(), 5000))
-                                console.error(err);
-                            })
+                        if(!member.bannable){
+                            embed.setDescription(`_El usuario no puede ser baneado._`).setColor('#ED4245')
+                            message.channel.send({embeds: [embed]})
+                                .then(msg => setTimeout(() => msg.delete(), 5000))
+                        } else {
+                            const reason = args.slice(1).join(' ')?args.slice(1).join(' '):'No se ha especificado una razón.';
+                            member.ban({days:7,reason:reason})
+                                .then(()=>{
+                                    embed.setDescription(`_${member.user.username}#${member.user.discriminator} ha sido baneado con motivo: \`${reason}\`_`).setColor('#57F287')
+                                    message.channel.send({embeds: [embed]})
+                                })
+                                .catch(err => {
+                                    embed.setDescription(`_No puedo banear a este usuario._`).setColor('#ED4245')
+                                    message.channel.send({embeds: [embed]})
+                                        .then(msg => setTimeout(() => msg.delete(), 5000))
+                                    console.error(err);
+                                })
+                        }
                     }
                 }
             }
