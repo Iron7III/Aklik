34e5ccc219341393c8d25ec668cc18a6fc65a6e2
Iron7III <aaronsjimenez@gmail.com>
2021-11-01 18:51:59 +0100

    aa
    Co-authored-by: Blue Banana <TheBlueBanana@users.noreply.github.com>

--- web/server.js
+++ web/server.js
@@ -2,21 +2,22 @@ const express = require('express');
 const app = express();
 console.web = (arg) => {console.log(`[WEB] ${arg}`);};
 
-const fs = require('fs');
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
 
 app.use(function (req, res, next) {
-    console.log('Time:', Date.now());
-    next();
-  });
+  	console.log('Time:', Date.now());
+  	next();
+});
 
 
 app.get('/', (req, res)=>{
-  //res.sendFile('web/index.html', {root : __dirname + '/views'});
-  res.send(fs.readFileSync('web/views/index.html'));
-  res.end();
+  	//res.sendFile('web/index.html', {root : __dirname + '/views'});
+  	res.sendFile(fs.readFileSync('web/views/index.html'));
+  	res.end();
 });
 
 const port = process.env.PORT || 80;
 app.listen(port, ()=>{
-    console.web(`Running server on port: ${port}`);
+  	console.web(`Running server on port: ${port}`);
 });
