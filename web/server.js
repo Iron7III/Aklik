1a22056e93372b8744e55c3c24a9b6107489a529
Iron7III <aaronsjimenez@gmail.com>
2021-11-01 20:08:12 +0100

    wafwafa

--- web/server.js
+++ web/server.js
@@ -25,17 +25,13 @@ app.get('/', (req, res)=>{
   	//res.sendFile('web/index.html', {root : __dirname + '/views'});
   	//res.render('/index.ejs');
     console.web(`Running on ${__dirname}`);
-  	//res.sendFile(__dirname + 'bhjbhjbhjbhjbhj/views/layout.ejs');
-    ejs.renderFile('/views/index.ejs', function(err, str){
-        console.web(str);
-        res.send(str);
-    });
-    ejs.renderFile('views/index.ejs', function(err, str){
-      	console.web(str);
+  	
+  	ejs.renderFile(__dirname + '/views/layout.ejs', function(err, strLayout){
+    	ejs.renderFile(__dirname + '/views/index.ejs', function(err, strIndex){
+        res.send(strLayout.replace('<%body%>', strIndex));
+        console.web(strLayout.replace('<%body%>', strIndex));
+      });
   	});
-  	ejs.renderFile(__dirname + '/views/index.ejs', function(err, str){
-    	console.web(str);
-	});
     res.end();
 });
 
