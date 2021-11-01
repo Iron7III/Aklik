1a11f09162e7acf4fcae8c6603d05a8d7193697c
Iron7III <aaronsjimenez@gmail.com>
2021-11-01 16:50:56 +0100

    change ;-;

--- web/server.js
+++ web/server.js
@@ -7,10 +7,6 @@ app.use(function (req, res, next) {
     console.log('Time:', Date.now());
     next();
   });
-app.get('/api/*', (req, res) => res.status(404).json({ code: 404 }));
-//app.all('*', (req, res) => res.status(200).sendFile(`web/public/index.html`));
-
-
 
 const port = process.env.PORT || 80;
 app.listen(port, ()=>{
